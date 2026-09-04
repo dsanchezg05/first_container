a20b5512895eaee9f7509d61602d6acd65e86351
dsanchezg05 <108696475+dsanchezg05@users.noreply.github.com>
2026-09-04 19:22:19 +0200

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -9,14 +9,20 @@ def prime_num(num):
      else:
         return True
    
-
+num_list = []
 while True:
     num = input("Write a number: ")
     if num == "exit":
         break
     else:
         num = int(num)
+        num_list.append(num)
     if prime_num(num) == True:
         print(f"{num} is Prime")
     else:
         print(f"{num} is not prime")
+
+with open("num_file.txt","w") as f:
+  for n in num_list:
+    f.write(f"{n}\n")
+
